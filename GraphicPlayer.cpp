ce2804f17d1b6bee333ead2fe7d9d73d2944bfbc
Calvin Ruiz <calvin.ruiz@epitech.eu>
2021-01-30 11:08:23 +0100

    Define core of GraphicPlayer

---
+++ GraphicPlayer.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** Game-Jam
+** File description:
+** GraphicPlayer.cpp
+*/
+#include "GraphicPlayer.hpp"
+
+GraphicPlayer::GraphicPlayer()
+{}
+
+GraphicPlayer::~GraphicPlayer()
+{}
---
+++ GraphicPlayer.hpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2020
+** Game-Jam
+** File description:
+** GraphicPlayer.hpp
+*/
+
+#ifndef GRAPHICPLAYER_HPP_
+#define GRAPHICPLAYER_HPP_
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+class GraphicPlayer {
+public:
+    enum orientation {
+        LEFT,
+        RIGHT,
+        TOP,
+        BOTTOM
+    };
+    GraphicPlayer();
+    virtual ~GraphicPlayer();
+    GraphicPlayer(const GraphicPlayer &cpy) = delete;
+    GraphicPlayer &operator=(const GraphicPlayer &src) = delete;
+
+    // define or redefine position
+    void setPosition(int x, int y);
+    // inform left movement
+    void left();
+    // inform right movement
+    void right();
+    // inform top movement
+    void top();
+    // inform bottom movement
+    void bottom();
+    // (from 0 to 1, 1 is 100% and 0 is 0%)
+    void setHealth(float value);
+    // set orientation
+    void setDirection(orientation dir);
+private:
+};
+
+#endif /* GRAPHICPLAYER_HPP_ */

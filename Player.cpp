958654fc141f511581dd7bffaee1cf735144862f
Calvin Ruiz <calvin.ruiz@epitech.eu>
2021-01-30 11:21:14 +0100

    Implement Player

---
+++ Player.cpp
@@ -0,0 +1,85 @@
+/*
+** EPITECH PROJECT, 2020
+** Game-Jam
+** File description:
+** Player.cpp
+*/
+#include "Player.hpp"
+#include "GraphicPlayer.hpp"
+#include "Room.hpp"
+#include "Core.hpp"
+
+Player::Player(GraphicPlayer *gPlayer) : gPlayer(gPlayer)
+{}
+
+Player::~Player()
+{}
+
+void Player::setPosition(int _x, int _y)
+{
+    x = _x;
+    y = _y;
+    gPlayer->setPosition(x, y);
+    pos = &Core::core->rooms[x][y];
+}
+
+void Player::left()
+{
+    dir = GraphicPlayer::LEFT;
+    gPlayer->setDirection(dir);
+    if (pos->left) {
+        gPlayer->left();
+        pos = &Core::core->rooms[--x][y];
+    }
+}
+
+void Player::right()
+{
+    dir = GraphicPlayer::RIGTH;
+    gPlayer->setDirection(dir);
+    if (pos->right) {
+        gPlayer->right();
+        pos = &Core::core->rooms[++x][y];
+    }
+}
+
+void Player::top()
+{
+    dir = GraphicPlayer::TOP;
+    gPlayer->setDirection(dir);
+    if (pos->top) {
+        gPlayer->top();
+        pos = &Core::core->rooms[x][--y];
+    }
+}
+
+void Player::bottom()
+{
+    dir = GraphicPlayer::BOTTOM;
+    gPlayer->setDirection(dir);
+    if (pos->bottom) {
+        gPlayer->bottom();
+        pos = &Core::core->rooms[x][++y];
+    }
+}
+
+void Player::useItem(int idx)
+{
+    if (inventory[idx]) {
+        // use item
+        if (inventory[idx]->use(x, y))
+            inventory[idx] = nullptr;
+    } else {
+        // collect item in the ground
+        Item *item = pos->item;
+        if (item && item->take())
+            inventory[idx] = item;
+    }
+}
+
+bool Player::update()
+{
+    if (pos && pos->hasCreeper)
+        health--;
+    return (health > 0);
+}
---
+++ Player.hpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2020
+** Game-Jam
+** File description:
+** Player.hpp
+*/
+
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <array>
+#include "GraphicPlayer.hpp"
+class room;
+
+class Player {
+public:
+    Player(GraphicPlayer *gPlayer);
+    virtual ~Player();
+    Player(const Player &cpy) = delete;
+    Player &operator=(const Player &src) = delete;
+
+    GraphicPlayer &get() {return *gPlayer;}
+    void setPosition(int x, int y);
+    void useItem(int idx);
+    // Damage player if walking on creeper, return true if alive, false is dead
+    bool update();
+    // movements
+    void left();
+    void right();
+    void top();
+    void bottom();
+private:
+    GraphicPlayer::orientation dir = GraphicPlayer::TOP;
+    room *pos = nullptr;
+    std::unique_ptr<GraphicPlayer> gPlayer;
+    std::array<Item *, 10> inventory;
+    int x, y, health;
+};
+
+#endif /* PLAYER_HPP_ */

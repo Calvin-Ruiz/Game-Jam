485558ffb690c651b29559a852ac6ca3f1c31b34
Calvin Ruiz <calvin.ruiz@epitech.eu>
2021-01-30 14:33:42 +0100

    Pre-build GraphicPlayer functions

--- GraphicPlayer.cpp
+++ GraphicPlayer.cpp
@@ -11,3 +11,24 @@ GraphicPlayer::GraphicPlayer()
 
 GraphicPlayer::~GraphicPlayer()
 {}
+
+// define or redefine position
+void GraphicPlayer::setPosition(int x, int y) {}
+
+// inform left movement
+void GraphicPlayer::left() {}
+
+// inform right movement
+void GraphicPlayer::right() {}
+
+// inform top movement
+void GraphicPlayer::top() {}
+
+// inform bottom movement
+void GraphicPlayer::bottom() {}
+
+// (from 0 to 1, 1 is 100% and 0 is 0%)
+void GraphicPlayer::setHealth(float value) {}
+
+// set orientation
+void GraphicPlayer::setDirection(orientation dir) {}
